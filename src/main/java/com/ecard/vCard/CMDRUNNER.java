d5918fa7447008ee70862ed26fb5d4a8f602c52e
Panji Maulana <panji.maulana.al@gmail.com>
2023-03-08 10:56:32 +0700

    c

--- src/main/java/com/ecard/vCard/CMDRUNNER.java
+++ src/main/java/com/ecard/vCard/CMDRUNNER.java
@@ -1,27 +1,27 @@
-package com.ecard.vCard;
+// package com.ecard.vCard;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.CommandLineRunner;
+// import org.springframework.stereotype.Component;
 
-import com.ecard.vCard.Entity.Person;
-import com.ecard.vCard.Repository.PersonRepository;
+// import com.ecard.vCard.Entity.Person;
+// import com.ecard.vCard.Repository.PersonRepository;
 
-@Component
-public class CMDRUNNER implements CommandLineRunner {
+// @Component
+// public class CMDRUNNER implements CommandLineRunner {
 
-    private static final Logger logger = LoggerFactory.getLogger(CMDRUNNER.class);
+//     private static final Logger logger = LoggerFactory.getLogger(CMDRUNNER.class);
 
-    @Autowired
-    private PersonRepository personRepository;
+//     @Autowired
+//     private PersonRepository personRepository;
 
-    @Override
-    public void run(String... args) throws Exception {
-        Person per = new Person(1, "Panji Maulana", "IT Application", "Panji@Gmail.com", "08123456789", null);
-            personRepository.save(per);
-        logger.info("Has been created");
-    }
+//     @Override
+//     public void run(String... args) throws Exception {
+//         Person per = new Person(1, "Panji Maulana", "IT Application", "Panji@Gmail.com", "08123456789", null);
+//             personRepository.save(per);
+//         logger.info("Has been created");
+//     }
     
-}
+// }
--- src/main/java/com/ecard/vCard/Controller/PersonController.java
+++ src/main/java/com/ecard/vCard/Controller/PersonController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,15 +19,15 @@ public class PersonController {
     private PersonRepository personRepository;
 
     @GetMapping("/Person")
-    public ResponseEntity<Map> Person(@RequestParam (required = true) Integer id){
+    public String Person(@RequestParam (required = true) Integer id, Model model){
         Map data = new HashMap<>();
         if (!personRepository.existsById(id)) {
-            data.put("message", "Data Tidak Ditemukan");
-            return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
-          }
-          Person person = personRepository.findById(id).get();
-        data.put("data", person);
-        return new ResponseEntity<>(data, HttpStatus.OK);
+            System.out.println("ID Tidak Ditemukan");
+        }
+        Person person = personRepository.findById(id).get();
+        model.addAttribute("person", person);
+        
+        return "index";
     }
     
 }
