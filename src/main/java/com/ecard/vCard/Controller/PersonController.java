2b57ec2a83e5b56d4a0c3cdece5839028e67178d
Panji Maulana <panji.maulana.al@gmail.com>
2023-03-08 11:20:25 +0700

    sd

--- src/main/java/com/ecard/vCard/Controller/PersonController.java
+++ src/main/java/com/ecard/vCard/Controller/PersonController.java
@@ -1,5 +1,6 @@
 package com.ecard.vCard.Controller;
 
+import java.sql.Blob;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ecard.vCard.Entity.Person;
@@ -24,8 +26,13 @@ public class PersonController {
         return "index";
     }
 
-    @GetMapping(value = "/Person")
-    public ResponseEntity<Map> Person(@RequestParam (required = true) Integer id){
+    @GetMapping(value = "/Register")
+    public String register() {
+        return "register";
+    }
+
+    @GetMapping(value = "/GetPerson")
+    public ResponseEntity<Map> GetPerson(@RequestParam (required = true) Integer id){
         Map data = new HashMap<>();
         if (!personRepository.existsById(id)) {
             System.out.println("GADA");
@@ -37,5 +44,10 @@ public class PersonController {
         data.put("data", person);
         return new ResponseEntity<>(data, HttpStatus.OK);
     }
+
+    // @PostMapping(value = "/InputPerson")
+    // public ResponseEntity<Map> InputPerson(@RequestParam String nama, @RequestParam String divisi, @RequestParam String email, @RequestParam String nowa, @RequestParam Blob foto) {
+
+    // }
     
 }
