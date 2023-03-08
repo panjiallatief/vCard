ef470b11a3fd8098ca3c9222db61b31e21b0a636
Panji Maulana <panji.maulana.al@gmail.com>
2023-03-08 11:06:14 +0700

    slebew

--- src/main/java/com/ecard/vCard/Controller/PersonController.java
+++ src/main/java/com/ecard/vCard/Controller/PersonController.java
@@ -6,25 +6,34 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ecard.vCard.Entity.Person;
 import com.ecard.vCard.Repository.PersonRepository;
 
+@Controller
 public class PersonController {
 
     @Autowired
     private PersonRepository personRepository;
 
-    @GetMapping("/Person")
+    @GetMapping(value = "/")
+    public String yoyo() {
+        return "index";
+    }
+
+    @GetMapping(value = "/Person")
     public ResponseEntity<Map> Person(@RequestParam (required = true) Integer id){
         Map data = new HashMap<>();
         if (!personRepository.existsById(id)) {
+            System.out.println("GADA");
             data.put("message", "Data Tidak Ditemukan");
             return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
           }
           Person person = personRepository.findById(id).get();
+          System.out.println("ADA");
         data.put("data", person);
         return new ResponseEntity<>(data, HttpStatus.OK);
     }
--- src/main/java/com/ecard/vCard/Repository/PersonRepository.java
+++ src/main/java/com/ecard/vCard/Repository/PersonRepository.java
@@ -1,9 +1,11 @@
 package com.ecard.vCard.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ecard.vCard.Entity.Person;
 
+@Repository
 public interface PersonRepository extends JpaRepository<Person, Integer> {
     
 }
