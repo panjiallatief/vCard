6e0c081a2e95ad0e81b2b81dac7cedc8ac9a657f
Panji Maulana <panji.maulana.al@gmail.com>
2023-03-08 10:57:49 +0700

    v

--- src/main/java/com/ecard/vCard/Controller/PersonController.java
+++ src/main/java/com/ecard/vCard/Controller/PersonController.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -19,15 +18,15 @@ public class PersonController {
     private PersonRepository personRepository;
 
     @GetMapping("/Person")
-    public String Person(@RequestParam (required = true) Integer id, Model model){
+    public ResponseEntity<Map> Person(@RequestParam (required = true) Integer id){
         Map data = new HashMap<>();
         if (!personRepository.existsById(id)) {
-            System.out.println("ID Tidak Ditemukan");
-        }
-        Person person = personRepository.findById(id).get();
-        model.addAttribute("person", person);
-        
-        return "index";
+            data.put("message", "Data Tidak Ditemukan");
+            return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
+          }
+          Person person = personRepository.findById(id).get();
+        data.put("data", person);
+        return new ResponseEntity<>(data, HttpStatus.OK);
     }
     
 }
