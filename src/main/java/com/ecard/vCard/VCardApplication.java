26e5d63bda644299d88282208efaa74ec4a1dc12
Panji Maulana Al Latief <65167200+panjiallatief@users.noreply.github.com>
2023-03-08 10:00:17 +0700

    first commit

---
+++ src/main/java/com/ecard/vCard/VCardApplication.java
@@ -0,0 +1,13 @@
+package com.ecard.vCard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VCardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecard/vCard/VCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecard.vCard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
