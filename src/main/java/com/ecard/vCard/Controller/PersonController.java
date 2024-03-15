65f502f467a99dfc1639f1df425c5788499f3281
Panji on Server <panji.maulana.al@gmail.com>
2024-03-15 15:51:01 +0700

    ..

--- src/main/java/com/ecard/vCard/Controller/PersonController.java
+++ src/main/java/com/ecard/vCard/Controller/PersonController.java
@@ -100,7 +100,7 @@ public class PersonController {
         model.addAttribute("Image", person.getImage());
         // model.addAttribute("gambar", person.getGambar());
  
-        return "indexcard"; 
+        return "index"; 
     }
 
     @PostMapping(value = "/InputPerson")
